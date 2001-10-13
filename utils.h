0ee081836a0dc9c41c14131aba11b889e5a2e680
endgame00 <endgame00>
2001-10-13 03:44:55 +0000

    New ASSERT macro - takes a string as an argument and pauses the process for debugging with gdb.

--- utils.h
+++ utils.h
@@ -1,3 +1,11 @@
 #include "vcfs.h"
 
 void split_path(vcfs_path path, vcfs_path *parent, vcfs_name *entry);
+
+#define ASSERT(assertion, s) if (!(assertion)) { \
+  fprintf(stderr, "****************************************************************\n"); \
+  fprintf(stderr, "ASSERT FAILED: Line %d in %s: %s\n", __LINE__, __FILE__, s); \
+  fprintf(stderr, "Please debug or kill the process\n"); \
+  fprintf(stderr, "****************************************************************\n"); \
+  fflush(stderr); pause(); }\
+  
